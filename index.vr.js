3286c369949ded319703f43deda9ede744773e1a
mark <mark.korput@gmail.com>
2017-11-16 13:17:15 +0100

    render textured-plane with transparency using cusotm shader

--- index.vr.js
+++ index.vr.js
@@ -9,13 +9,34 @@ import {
 } from 'react-vr';
 
 export default class ReactVrTest extends React.Component {
+
   render() {
     return (
       <View>
         <Pano source={asset('chess-world.jpg')}/>
-        <Plane dimWidth={2} dimHeight={1} texture={asset('chess-world.jpg')} style={{
-          transform: [{translate: [0, 0, -2]}]
-        }}/>
+
+
+        <Plane dimWidth={2} dimHeight={1}
+          texture={asset('chess-world.jpg')}
+          materialParameters={{
+             fragmentShader: '\
+             uniform sampler2D   tex0;\
+             varying vec2  vUv;\
+\
+             void main()	{\
+               vec4 color = texture2D( tex0, vUv + vec2(0.0,0.0));\
+               gl_FragColor = vec4(vec3(color), 0.8);\
+             }\
+             ',
+             vertexShader: '    varying vec2 vUv;\
+                 void main()	{\
+                   vUv = uv;\
+                   gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );\
+                 }',
+             transparent: true,
+          }}
+          style={{
+            transform: [{translate: [0, 0, -2]}]}}/>
 
         {/* <Text
           style={{
@@ -31,8 +52,7 @@ export default class ReactVrTest extends React.Component {
           }}>
           hello
         </Text> */}
-      </View>
-    );
+      </View>);
   }
 };
 
